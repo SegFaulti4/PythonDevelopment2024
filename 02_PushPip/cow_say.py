c728bdd4c4834e7bae3bf9e721e0b528e4babe48
SegFaulti4 <makar.popov01@mail.ru>
2024-02-26 17:33:33 +0300

    Help messages for all arguments, added missing dead preset, applied "black" formatting

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -7,19 +7,71 @@ import textwrap
 def arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("message", nargs="*", type=str)
-    parser.add_argument("-l", dest="list_cows", action="store_true", help="list all cows")
-    parser.add_argument("-n", dest="wrap_text", action="store_false", help="do not wrap text, if specified any positional arguments are prohibited")
-    parser.add_argument("-f", dest="cow", action="store", default="default", type=str)
-    parser.add_argument("-e", dest="eyes", action="store", default="OO", type=str)
-    parser.add_argument("-T", dest="tongue", action="store", default="  ", type=str)
-    parser.add_argument("-W", dest="width", action="store", default=40, type=int)
-    parser.add_argument("-b", const="b", dest="preset", action="append_const")
-    parser.add_argument("-g", const="g", dest="preset", action="append_const")
-    parser.add_argument("-p", const="p", dest="preset", action="append_const")
-    parser.add_argument("-s", const="s", dest="preset", action="append_const")
-    parser.add_argument("-t", const="t", dest="preset", action="append_const")
-    parser.add_argument("-w", const="w", dest="preset", action="append_const")
-    parser.add_argument("-y", const="y", dest="preset", action="append_const")
+    parser.add_argument(
+        "-l", dest="list_cows", action="store_true", help="list all cows"
+    )
+    parser.add_argument(
+        "-n",
+        dest="wrap_text",
+        action="store_false",
+        help="do not wrap text, if specified any positional arguments are prohibited",
+    )
+    parser.add_argument(
+        "-f",
+        dest="cow",
+        action="store",
+        default="default",
+        type=str,
+        help="name of the cow",
+    )
+    parser.add_argument(
+        "-e",
+        dest="eyes",
+        action="store",
+        default="OO",
+        type=str,
+        help="custom eye string",
+    )
+    parser.add_argument(
+        "-T",
+        dest="tongue",
+        action="store",
+        default="  ",
+        type=str,
+        help="custom tongue string",
+    )
+    parser.add_argument(
+        "-W",
+        dest="width",
+        action="store",
+        default=40,
+        type=int,
+        help="with of the text bubble",
+    )
+    parser.add_argument(
+        "-b", const="b", dest="preset", action="append_const", help="Borg preset"
+    )
+    parser.add_argument(
+        "-d", const="d", dest="preset", action="append_const", help="dead preset"
+    )
+    parser.add_argument(
+        "-g", const="g", dest="preset", action="append_const", help="greedy preset"
+    )
+    parser.add_argument(
+        "-p", const="p", dest="preset", action="append_const", help="paranoia preset"
+    )
+    parser.add_argument(
+        "-s", const="s", dest="preset", action="append_const", help="stoned preset"
+    )
+    parser.add_argument(
+        "-t", const="t", dest="preset", action="append_const", help="tired preset"
+    )
+    parser.add_argument(
+        "-w", const="w", dest="preset", action="append_const", help="wired preset"
+    )
+    parser.add_argument(
+        "-y", const="y", dest="preset", action="append_const", help="youthful preset"
+    )
     return parser
 
 
